2e7dce9b13644cadea34b7418c92e8bd12bf88bd
efimenko bohdan <bogdane@weblium.com>
2019-05-07 11:01:35 +0300

    add files

--- src/favorites.js
+++ src/favorites.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Favorites = () => {
-  return (
-    <h2>Favorites</h2>
-  )
-}
+  const [favorites, setFavorites] = useState({});
 
-export default Favorites
+  useEffect(() => {
+    const favoritesFromStorage = JSON.parse(localStorage.getItem("favorites"));
+    setFavorites(favoritesFromStorage);
+  }, []);
+
+  return (
+    <React.Fragment>
+      <h2>Favorites</h2>
+      {Object.keys(favorites).length !== 0 && (
+        <ul>
+          {Object.keys(favorites).map(filmId => {
+            return (
+              <li>
+                <Link to={`/film/${filmId}`}>{favorites[filmId].title}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default Favorites;
--- src/list.js
+++ src/list.js
@@ -1,17 +1,36 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-const List = ({data}) => {
-  console.log({data})
-  return data && (
-    <ul className="films-list">
-      {data.map(({ title, id }) => {
-        return <li key={id} className="films-list__item">
-          <Link to={`/film/${id}`}>{title}</Link>
-        </li>;
-      })}
-    </ul>
-  )
-}
+const List = ({ data }) => {
+  const initialFavotites = JSON.parse(localStorage.getItem("favorites")) || {};
+  const [favorites, setFavorites] = useState(initialFavotites);
 
-export default List
+  const addToFavorite = filmObject => () => {
+    setFavorites({...favorites, ...filmObject});
+  };
+
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
+
+  return (
+    data && (
+      <ul className="films-list">
+        {data.map(({ title, id }) => {
+          return (
+            <li key={id} className="films-list__item">
+              <Link to={`/film/${id}`}>{title}</Link>
+              {!Object.keys(favorites).includes(String(id)) && (
+                <button type="button" onClick={addToFavorite({[id]: {title}})}>
+                  Add to favorites
+                </button>
+              )}
+            </li>
+          );
+        })}
+      </ul>
+    )
+  );
+};
+
+export default List;
