635b13f6a24cb1aa576a76cbbf15ca4203c46217
efimenko bohdan <bogdane@weblium.com>
2019-05-10 09:27:50 +0300

    refactor favorite button

--- src/components/favorite-button/index.js
+++ src/components/favorite-button/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import classNames from 'classnames'
 import "./style.css";
 
 const FavoriteButton = ({
@@ -7,37 +8,21 @@ const FavoriteButton = ({
   isFavorite,
   id,
   film
-}) =>
-  !isFavorite ? (
+}) => (
     <button
       type="button"
       className="favorite-button film-item__favorite"
-      onClick={addToFavorite({ [id]: film })}
-      title="Add to favorite"
+      onClick={isFavorite ? removeFromFavorite(id) : addToFavorite({ [id]: film })}
+      title={isFavorite ? "Remove from favorite" : "Add to favorite"}
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 53.9 53.9"
-        className="favorite-icon"
+        className={classNames("favorite-icon", {'favorite-icon--active': isFavorite})}
       >
         <path d="M27 1.3l8.3 16.9 18.6 2.7L40.4 34l3.2 18.5-16.7-8.7-16.6 8.7L13.5 34 0 21l18.6-2.7z" />
       </svg>
     </button>
-  ) : (
-    <button
-      type="button"
-      className="favorite-button film-item__favorite"
-      onClick={removeFromFavorite(id)}
-      title="Remove from favorite"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 53.9 53.9"
-        className="favorite-icon favorite-icon--active"
-      >
-        <path d="M27 1.3l8.3 16.9 18.6 2.7L40.4 34l3.2 18.5-16.7-8.7-16.6 8.7L13.5 34 0 21l18.6-2.7z" />
-      </svg>
-    </button>
-  );
+  )
 
 export default FavoriteButton;
